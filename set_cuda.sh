72516f68cee3e461e88a3fa27466192807ff5ff4
Robert Belanec <belanecrobert22@gmail.com>
2022-04-12 12:02:23 +0200

    added submodule

---
+++ set_cuda.sh
@@ -0,0 +1,1 @@
+export CUDA_PATH=/usr/local/cuda-11.2
--- test_torch.py
+++ test_torch.py
@@ -1,3 +1,11 @@
 import torch
-
 print('Current device:', torch.cuda.get_device_name(torch.cuda.current_device()))
+
+from torch.utils.cpp_extension import CUDA_HOME
+print(CUDA_HOME)
+
+#import os
+#print(os.environ.get('CUDA_PATH'))
+
+#model = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', pretrained=True)
+#model.eval()
