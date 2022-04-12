d5ce683958c169bc05f4c460943ad440dc1cb6b3
Robert Belanec <belanecrobert22@gmail.com>
2022-04-12 19:26:45 +0200

    generating samples updated

--- generate_samples.py
+++ generate_samples.py
@@ -5,6 +5,18 @@ import numpy as np
 
 import legacy
 
+def make_transform(translate: Tuple[float,float], angle: float):
+    m = np.eye(3)
+    s = np.sin(angle/360.0*np.pi*2)
+    c = np.cos(angle/360.0*np.pi*2)
+    m[0][0] = c
+    m[0][1] = s
+    m[0][2] = translate[0]
+    m[1][0] = -s
+    m[1][1] = c
+    m[1][2] = translate[1]
+    return m
+
 network_pkl = 'https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/stylegan3-t-ffhq-1024x1024.pkl'
 
 device = torch.device('cuda')
