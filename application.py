daccc521c76b77af2f8d95a6d6d8f441f544a8e6
Robert Belanec <belanecrobert22@gmail.com>
2022-04-12 20:43:43 +0200

    added templates dir

--- application.py
+++ application.py
@@ -4,8 +4,7 @@ import os
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './samples'
 
-@app.route('/')
-@app.route('/index')
+@app.route('/<name>')
 def show_index():
-	full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'image0001.png')
+	full_filename = os.path.join(app.config['UPLOAD_FOLDER'], '{name}')
 	return render_template('index.html', user_image=full_filename)
