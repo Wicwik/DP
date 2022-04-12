be8562b1375769324ea095a8dc8562bec73fbfd1
Robert Belanec <belanecrobert22@gmail.com>
2022-04-12 20:18:04 +0200

    added application.py

---
+++ application.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'samples'
+
+@app.route('/')
+@app.route('/index')
+def show_index():
+	full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'image0001.png')
+	return render_template("index.html", user_image=full_filename)
