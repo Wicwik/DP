f8c52c4660544e5d5018c65981678cc5a5642885
Robert Belanec <belanecrobert22@gmail.com>
2022-04-12 20:48:16 +0200

    added templates dir

--- application.py
+++ application.py
@@ -3,9 +3,7 @@ from markupsafe import escape
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './samples'
 
-@app.route('/<name>')
+@app.route('/')
 def show_image(name):
-	full_filename = os.path.join(app.config['UPLOAD_FOLDER'], '{escape(name)}')
-	return render_template('index.html', user_image=full_filename)
+	return render_template('index.html', user_image='samples/image0001.png')
