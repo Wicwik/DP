61d12a76e4f5b421561c0aa221c7e658477d4357
Robert Belanec <belanecrobert22@gmai.com>
2022-05-08 22:44:00 +0200

    classifier training works

--- celeba_classifier.py
+++ celeba_classifier.py
@@ -121,17 +121,16 @@ def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
-    test_loss, correct = 0, 0
+    test_loss, correct = 0, []
     with torch.no_grad():
         for X, y in dataloader:
             X, y = X.to(device), y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y.float()).item()
             print(torch.round(pred), y)
-            correct += (torch.round(pred) == y).type(torch.float).sum().item()
+            correct.append((torch.round(pred) == y).type(torch.float).sum().item()/len(y))
     test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Test Error: \n Accuracy: {(100*torch.mean(correct)):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
 epochs = 5
