6b7d93ebed9f48fdd046b5a513e0665ef23dee19
Robert Belanec <belanecrobert22@gmai.com>
2022-05-08 21:18:22 +0200

    classifier training works

--- celeba_classifier.py
+++ celeba_classifier.py
@@ -121,6 +121,7 @@ def test(dataloader, model, loss_fn):
             X, y = X.to(device), y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y.float()).item()
+            print(pred.argmax(1))
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
