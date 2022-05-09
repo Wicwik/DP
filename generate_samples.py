a61450c30b9488296775b95c0853ee1866783dc0
Robert Belanec <belanecrobert22@gmail.com>
2022-05-09 10:30:17 +0200

    generating samples

--- generate_samples.py
+++ generate_samples.py
@@ -1,10 +1,9 @@
 from stylegan_generator import StyleGANGenerator
 
-
 network_pkl = 'https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/stylegan3-r-ffhqu-1024x1024.pkl'
 generator = StyleGANGenerator(network_pkl)
 
-n_samples = 10
+n_samples = 256000
 outdir = '/data/users/rbelanec/generated_images'
 
 generator.generate_images(n_samples, outdir, truncation_psi=0.6)
