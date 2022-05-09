9b8071cdd32c9401e7fc87bd67555dfbb29c839c
Robert Belanec <belanecrobert22@gmail.com>
2022-05-09 10:47:36 +0200

    updated screen killing

--- run_app.sh
+++ run_app.sh
@@ -1,4 +1,4 @@
-screen -ls | grep -i detached | cut -d. -f1 | awk '{print $1}' | xargs kill &> /dev/null
+screen -ls | grep -i detached | grep flask | cut -d. -f1 | awk '{print $1}' | xargs kill &> /dev/null
 
 export FLASK_APP=application
 screen -S flask_app -d -m flask run --host=0.0.0.0 --port=8002
